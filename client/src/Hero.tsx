435335de70792ec26835548412fb29e462723e5e
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-10-15 14:25:27 +0200

    feat: edit a name (frontend)

--- client/src/Hero.tsx
+++ client/src/Hero.tsx
@@ -1,3 +1,4 @@
+import { createSignal, Show } from "solid-js";
 import styles from "./App.module.css";
 
 interface Hero {
@@ -6,9 +7,43 @@ interface Hero {
 }
 
 export const Hero = (props) => {
+  const [editing, setEditing] = createSignal(false);
+  const [name, setName] = createSignal(props.hero.name);
+
+  const handleSubmit = (heroId, evt) => {
+    evt.preventDefault();
+    setEditing(false);
+  };
+
   return (
-    <li>
-      <span class={styles.hero_id}>{props.hero.id}</span> {props.hero.name}
+    <li class={styles.hero}>
+      <Show
+        when={editing()}
+        fallback={
+          <>
+            <span class={styles.hero_id}>{props.hero.id}</span> {name()}
+            <div>
+              <button
+                class={styles.hero_button}
+                onClick={() => setEditing(true)}
+              >
+                🖉
+              </button>
+              <button class={styles.hero_delete}>🗶</button>
+            </div>
+          </>
+        }
+      >
+        <form action="/" onSubmit={[handleSubmit, props.hero.id]}>
+          <label for="hero-name">Hero Name</label>
+          <input
+            id="hero-name"
+            value={name()}
+            type="text"
+            onInput={(evt) => setName(evt.target.value)}
+          />
+        </form>
+      </Show>
     </li>
   );
 };
